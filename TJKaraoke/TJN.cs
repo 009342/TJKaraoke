1993c209230f9154775647f50f69696084e8e65c
뇌씨 <009342@naver.com>
2020-09-17 04:15:04 +0900

    곡 정보 추가 및 기타 코드 수정

--- TJKaraoke/TJN.cs
+++ TJKaraoke/TJN.cs
@@ -5,18 +5,17 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace TJKaraoke
 {
-    class TJN
+    public class TJN
     {
-        [DllImport("msvcrt.dll", CallingConvention = CallingConvention.Cdecl)]
-        static extern int memcmp(byte[] b1, byte[] b2, long count);
+        //[DllImport("msvcrt.dll", CallingConvention = CallingConvention.Cdecl)]
+        //static extern int memcmp(byte[] b1, byte[] b2, long count);
         static bool ByteArrayCompare(byte[] b1, byte[] b2)
         {
-            return b1.Length == b2.Length && memcmp(b1, b2, b1.Length) == 0;
-        }
+            return b1.Length == b2.Length && b1.SequenceEqual(b2);
+        }//운영체제 의존성 코드 수정
         //private static byte[] magic = { 0x54, 0x41, 0x49, 0x4A, 0x49, 0x4E, 0x20, 0x4D, 0x45, 0x44, 0x49, 0x41, 0x20, 0x43, 0x4F, 0x2E, 0x2C, 0x4C, 0x54, 0x44,
         //    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
         //매직이 다른 경우가 있기에 앞 16바이트만 검사
@@ -26,20 +25,28 @@ namespace TJKaraoke
         private static string[] specialChars = { "@", "(", ")", "っ", "ん", "ー", "ン", "ッ", "　", " ", "ョ", "ょ", "ゃ", "ャ", "ゅ", "ュ", "ぁ", "ァ", "ぃ", "ィ", "ぅ", "ゥ", "ぇ", "ェ", "ぉ", "ォ", "[", "]", "/", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
         private static string[] specialChars2 = { "]", "(" };
         //한 덩어리로 처리해야 할 특수한 문자들
-        public string path { get; private set; }
-        public int country { get; private set; }
-        public int midiSize { get; private set; }
-        public int xcgSize { get; private set; }
-        public int chordSize { get; private set; }
-        public byte[] midi { get; private set; }
-        public byte[] xcg { get; private set; }
-        public byte[] chord { get; private set; }
-        public short lyricsSize { get; private set; }
-        public short syncSize { get; private set; }
-        public byte[] syncData { get; private set; }
-        public byte[] lyricsData { get; private set; }
-        public SongInfo lyrics { get; private set; }
-        public Encoding encoding { get; private set; }
+        private static string[] specialChars3 = { "　", " " };
+        //무조건 루프를 끊어주야 할 특수한 문자들
+
+        private static char[] specialCharsc = { '@', ')', 'っ', 'ん', 'ー', 'ン', 'ッ', 'ョ', 'ょ', 'ゃ', 'ャ', 'ゅ', 'ュ', 'ぁ', 'ァ', 'ぃ', 'ィ', 'ぅ', 'ゥ', 'ぇ', 'ェ', 'ぉ', 'ォ', '[', ']', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
+        ,'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
+        ,'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','\'','　', ' ' }; //(는 나오면 덩어리로 처리해줘야 하니 예외,
+        private static char[] specialCharsc2 = { '　', ' ' }; //La la이런 경우에는 잘라주어야 겠지
+        private static char[] specialCharsc3 = { '(' }; //전에 문자를 확인할 필요가 있을 경우, 전 문자가 ( 인 경우
+        public string Path { get; private set; }
+        public int Country { get; private set; }
+        public int MidiSize { get; private set; }
+        public int XcgSize { get; private set; }
+        public int ChordSize { get; private set; }
+        public byte[] MidiData { get; private set; }
+        public byte[] XcgData { get; private set; }
+        public byte[] ChordData { get; private set; }
+        public short LyricsSize { get; private set; }
+        public short SyncSize { get; private set; }
+        public byte[] SyncData { get; private set; }
+        public byte[] LyricsData { get; private set; }
+        public SongInfo Lyrics { get; private set; }
+        public Encoding Encoding { get; private set; }
         public TJN(string path, int country, bool isCompressed)
         {
 
@@ -47,8 +54,8 @@ namespace TJKaraoke
             {
                 throw new NotImplementedException("");
             }
-            this.country = country;
-            this.path = path;
+            this.Country = country;
+            this.Path = path;
             using (FileStream fstream = File.OpenRead(path))
             {
                 byte[] buffer = new byte[0x10];
@@ -59,20 +66,20 @@ namespace TJKaraoke
                 buffer = new byte[4];
 
                 fstream.Read(buffer, 0, 4);
-                midiSize = BitConverter.ToInt32(buffer, 0);
+                MidiSize = BitConverter.ToInt32(buffer, 0);
                 fstream.Read(buffer, 0, 4);
-                xcgSize = BitConverter.ToInt32(buffer, 0);
+                XcgSize = BitConverter.ToInt32(buffer, 0);
                 fstream.Read(buffer, 0, 4);
-                chordSize = BitConverter.ToInt32(buffer, 0);
+                ChordSize = BitConverter.ToInt32(buffer, 0);
                 fstream.Position += 8;
-                midi = new byte[midiSize];
-                fstream.Read(midi, 0, midiSize);
-                xcg = new byte[xcgSize];
-                fstream.Read(xcg, 0, xcgSize);
-                chord = new byte[chordSize];
-                fstream.Read(chord, 0, chordSize);
+                MidiData = new byte[MidiSize];
+                fstream.Read(MidiData, 0, MidiSize);
+                XcgData = new byte[XcgSize];
+                fstream.Read(XcgData, 0, XcgSize);
+                ChordData = new byte[ChordSize];
+                fstream.Read(ChordData, 0, ChordSize);
 
-                using (Stream stream = new MemoryStream(xcg))
+                using (Stream stream = new MemoryStream(XcgData))
                 {
                     buffer = new byte[8];
                     stream.Read(buffer, (int)stream.Position, 8);
@@ -166,12 +173,12 @@ namespace TJKaraoke
                                         t.str = word + (seperator == "_" ? "" : seperator);
                                         line = line.Substring(t.str.Length + (seperator == "_" ? 1 : 0));
                                     }
-                                    if (pronContinue)
+                                    if (pronContinue) //요미가나의 연속성을 위해
                                     {
-                                        t.pronGuide = tickEvents.Last((last) =>
+                                        t.PronGuide = tickEvents.Last((last) =>
                                         {
                                             return last.cmd == 0x01;
-                                        }).pronGuide;
+                                        }).PronGuide;
                                         if (line.Substring(1, 1) == "＞")
                                         {
                                             t.str = line.Substring(0, 1);
@@ -194,7 +201,9 @@ namespace TJKaraoke
                                         }
                                         else break;
                                     }
-                                    if (line.Length > 0 && line.Substring(0, 1) == "＜")
+
+                                    if (!(t.str.Length > 0 && specialChars3.Contains(t.str.Substring(t.str.Length - 1))) //asdf ＜発音＞과 같은 경우를 위해 
+                                        && line.Length > 0 && line.Substring(0, 1) == "＜")
                                     {
                                         word = line.Substring(1).Split('＞')[0];
                                         if (Regex.IsMatch(word, regexString) || word.Length == 1)
@@ -205,17 +214,18 @@ namespace TJKaraoke
                                         else
                                         {
                                             pronContinue = true;
-                                            t.pronGuide = new PronGuide();
+                                            t.PronGuide = new PronGuide();
                                             line = line.Substring(1, line.Length - 1);
                                             t.str += line.Substring(0, 1);
                                             line = line.Substring(1, line.Length - 1);
                                         }
                                     }
-                                    if (line.Length > 0)
+                                    if (line.Length > 0) //다음 문자열 가져오고 대비
                                     {
                                         bool loop = true;
                                         while (loop)
                                         {
+
                                             if (line.Length > 0 && t.str == "")
                                             {
                                                 t.str += line.Substring(0, 1);
@@ -223,15 +233,16 @@ namespace TJKaraoke
                                             }
                                             if (line.Length > 0 && specialChars.Contains(line.Substring(0, 1)))
                                             {
+                                                if (specialChars3.Contains(line.Substring(0, 1))) loop = false;
                                                 t.str += line.Substring(0, 1);
                                                 line = line.Substring(1, line.Length - 1);
                                             }
                                             else if (line.Length > 0 && line.Substring(0, 1) == "「")
                                             {
-                                                if (t.pronGuide == null)
-                                                    t.pronGuide = new PronGuide();
-                                                t.pronGuide.Pron = line.Split('「')[1].Split('」')[0];
-                                                line = line.Substring((t.pronGuide.Pron.Length + 2), line.Length - (t.pronGuide.Pron.Length + 2));
+                                                if (t.PronGuide == null)
+                                                    t.PronGuide = new PronGuide();
+                                                t.PronGuide.Pron = line.Split('「')[1].Split('」')[0];
+                                                line = line.Substring((t.PronGuide.Pron.Length + 2), line.Length - (t.PronGuide.Pron.Length + 2));
                                             }
                                             else
                                             {
@@ -252,27 +263,66 @@ namespace TJKaraoke
                             }
 
                         }
+
                     }
                 }
+                /*
+                int part = 3;
+                TickEvent temp = null;
+                foreach (TickEvent item in Lyrics.tickEvents)
+                {
+                    if (temp == null)
+                    {
+                        item.part = 3;
+                    }
+                    if (item.str.Contains("[/"))
+                    {
+                        item.part = part;
+                        part = 3;
+                        item.str = Regex.Replace(item.str, @" ?\[.*?\]", string.Empty);
+                    }
+                    else if (item.str.Contains("["))
+                    {
+                        part = int.Parse(item.str.Split('[')[1].Split(']')[0]);
+                        item.part = part;
+                        item.str = Regex.Replace(item.str, @" ?\[.*?\]", string.Empty);
+                    }
+                    else
+                    {
+                        item.part = part;
+                    }
+
+                }
+                                데이터의 일관성을 해칠 염려가 있을려나,
+                */
             }
         }
     }
-    class SongInfo
+    public class SongInfo
     {
         public int kNumber;
-        public string name;
+        public string title;
+        public string subTitle;
         public string composer;
         public string lyricist;
         public string singer;
         public List<TickEvent> tickEvents;
+        public string HangulLyrics = null;
     }
-    class TickEvent
+    public class TickEvent
     {
         public int tick;
         public byte cmd;
         public string str = "";
+        //public int part = 3;
+        /*
+         * 0 : 여자
+         * 1 : 남자
+         * 2 : 혼성
+         * 3 : 기본
+         */
         private PronGuide privPronGuide;
-        public PronGuide pronGuide
+        public PronGuide PronGuide
         {
             get
             {
@@ -280,15 +330,18 @@ namespace TJKaraoke
             }
             set
             {
-                privPronGuide = value;
-                privPronGuide.Ref = new List<TickEvent>();
-                privPronGuide.Ref.Add(this);
+                if (value != null)
+                {
+                    privPronGuide = value;
+                    privPronGuide.Ref = new List<TickEvent>();
+                    privPronGuide.Ref.Add(this);
+                }
             }
         }
         public int lineNumber = 0;
         public int indexOfLine = 0;
     }
-    class PronGuide
+    public class PronGuide
     {
         public string Pron;
         public List<TickEvent> Ref;
